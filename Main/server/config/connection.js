c6ba330b601522c2be413c49e6ab5e640084ef39
Juan <j.v.delcruz70120@gmail.com>
2023-10-02 17:48:37 -0700

    updated graphql

--- Main/server/config/connection.js
+++ Main/server/config/connection.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false);
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/<DB_fitness>';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/fitnessplus';
 
 connect(connectionString, {
   useNewUrlParser: true,
--- Main/server/seeds/seed.js
+++ Main/server/seeds/seed.js
@@ -5,49 +5,21 @@ const cardioData = require('./cardioData.json');
 const resistanceData = require('./resistanceData.json');
 const userData = require('./userData.json');
 
-async function seedDatabase() {
+db.once('open', async () => {
   try {
-    // Remove existing data from collections
     await Cardio.deleteMany({});
     await Resistance.deleteMany({});
     await User.deleteMany({});
 
-    // Insert cardio and resistance data into the database
-    const cardios = await Cardio.insertMany(cardioData);
-    const resistances = await Resistance.insertMany(resistanceData);
+    await Cardio.create({cardioData});
+    await Resistance.create({resistanceData});
+    await User.create({userData});
 
-    // Associate cardio and resistance data with users
-    const users = await User.insertMany(userData);
 
-    // Loop through users and assign random exercises
-    users.forEach(async (user) => {
-      const randomCardios = getRandomExercises(cardios);
-      const randomResistances = getRandomExercises(resistances);
-
-      user.cardio = randomCardios.map((exercise) => exercise._id);
-      user.resistance = randomResistances.map((exercise) => exercise._id);
-
-      await user.save();
-    });
-
-    console.log('Database seeded successfully.');
-  } catch (error) {
-    console.error('Error seeding the database:', error);
-  } finally {
-    // Close the database connection
-    db.close();
+    console.log('all done!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
   }
-}
+});
 
-function getRandomExercises(exerciseArray) {
-  const numberOfExercises = Math.floor(Math.random() * exerciseArray.length) + 1;
-  const randomExercises = [];
-  for (let i = 0; i < numberOfExercises; i++) {
-    const randomIndex = Math.floor(Math.random() * exerciseArray.length);
-    randomExercises.push(exerciseArray[randomIndex]);
-  }
-  return randomExercises;
-}
-
-// Call the seedDatabase function to populate the database
-seedDatabase();
